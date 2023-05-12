8c1ba6e2f1e85fba6338170f4a4647abe862076e
Ben Goshow <goshow@heysparkbox.com>
2023-05-12 12:25:22 -0400

    feat: imports function and adds test

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 import '@/scss/base.scss';
+import { sum } from './sum.js';
 
-console.log('test');
+const a = 1;
+const b = 2;
+
+document.getElementById('app').innerHTML = `
+  <h1>${a} + ${b} = ${sum(a, b)}</h1>
+`;
